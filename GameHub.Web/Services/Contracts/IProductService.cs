2554abc4639f57866489bbdf6ea962025e920105
Master <jairojunior955@gmail.com>
2023-09-15 12:56:30 -0300

    feat: implementação interface IProductService

---
+++ GameHub.Web/Services/Contracts/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using GameHub.Web.Models;
+
+namespace GameHub.Web.Services.Contracts
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductViewModel>> GetAllProducts();
+        Task<IEnumerable<ProductViewModel>> FindProductById(int id);
+        Task<IEnumerable<ProductViewModel>> CreateProduct(ProductViewModel productVM);
+        Task<IEnumerable<ProductViewModel>> UpdateProduct(ProductViewModel productVM);
+        Task<bool> DeleteById(int id);
+
+    }
+}
