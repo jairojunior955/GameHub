ca08e4df23b18c2fadc5d5ca391849b06cec1ed0
Master <jairojunior955@gmail.com>
2023-09-15 08:43:33 -0300

    fix: Serialização do ProductDTO

--- GameHub.ProductAPI/Program.cs
+++ GameHub.ProductAPI/Program.cs
@@ -15,8 +15,7 @@ namespace GameHub.ProductAPI
 
             // Add services to the container.
 
-            builder.Services.AddControllers().AddJsonOptions(
-                x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+            builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
